0495889240535318650674b858a1846925d5205e
Umair Abid <46405345+OmairAbid@users.noreply.github.com>
2023-02-23 19:26:55 +0500

    Update AddConnector.cs

--- src/Services/ConfigurationService/Core/Application.Commands/Features/Connectors/Add/AddConnector.cs
+++ src/Services/ConfigurationService/Core/Application.Commands/Features/Connectors/Add/AddConnector.cs
@@ -46,53 +46,14 @@ public class AddConnectorRequestHandler : IRequestHandler<AddConnectorRequest, B
     public async Task<BasicResponse<Connector>> Handle(AddConnectorRequest request, CancellationToken cancellationToken)
     {
 
-        //TODO : Add Role Check to make sure user has rights to Add the Connector.
-        //TODO : Pre Request data Validation
-
         BasicResponse<Connector> response = new();
-        Connector _connector;
-        ConnectorDetail _connectorDetail;
-        _connector = new Connector
-        {
-            Status = request.Status ? (int)Status.ENABLED : (int)Status.DISABLED,
-            Name = request.ConnectorName,
-            //IsForADSS = request.IsForADSS == 1,
-            Provider = request.ProviderParams.AttributeKey,
-            Identifier = request.ProviderParams.AttributeName,
-            HMAC = request.ProviderParams.HMAC ?? DatabaseStaticData.HMAC,
-            CreatedBy = request.UserEmail ?? request.ProviderParams.CreatedBy ?? DatabaseStaticData.ADMIN,
-            LastModifiedBy = request.UserEmail ?? request.ProviderParams.LastModifiedBy ?? DatabaseStaticData.ADMIN,
-            LastModifiedOn = request.ProviderParams.LastModifiedOn ?? DateTime.Now,
-            ConnectorDetail = new List<ConnectorDetail>(),
-
-        };
-        request.ProviderParams.ConnectionProviderParameter.ForEach((item) =>
-        {
-            _connectorDetail = new ConnectorDetail
-            {
-                
-                AttributeName = item.AttributeKey,
-                AttributeValue = item.AttributeValue != null ? item.AttributeValue.Trim() : string.Empty,
-                //SortOrder = item.SortOrder,
-                Type = item.Type,
-                CreatedBy = request.UserEmail?? item.CreatedBy?? DatabaseStaticData.ADMIN,
-                LastModifiedBy = request.UserEmail?? item.LastModifiedBy?? DatabaseStaticData.ADMIN,
-                LastModifiedOn = item.LastModifiedOn ?? DateTime.Now,
-                HMAC = item.HMAC ?? DatabaseStaticData.HMAC,
-            };
-
-            _connector.ConnectorDetail.Add(_connectorDetail);
-        });
-
-       Connector connector = await _connectorCommandRepository.AddAsync(_connector);
-
+        
         if (await _unitOfWork.CommitAsync(cancellationToken))
         {
             response.Data = connector;
             response.Success = true;
         }
 
-        response.Data = _connector.DeepClone();
         response.Success = true;
         
         return response;
