2927b827e0e62b4cd14205552318d380e35fe9be
Umair Abid <46405345+OmairAbid@users.noreply.github.com>
2023-02-23 19:27:55 +0500

    Update UpdateConnector.cs

--- src/Services/ConfigurationService/Core/Application.Commands/Features/Connectors/Update/UpdateConnector.cs
+++ src/Services/ConfigurationService/Core/Application.Commands/Features/Connectors/Update/UpdateConnector.cs
@@ -48,47 +48,9 @@ public class UpdateConnectorRequestHandler : IRequestHandler<UpdateConnectorRequ
     public async Task<BasicResponse> Handle(UpdateConnectorRequest request, CancellationToken cancellationToken)
     {
 
-        //TODO : Add Role Check to make sure user has rights to Update the Connector.
-        //TODO : Pre Request data Validation
-
-        //TODO : Disucss and remove additional things from command model
+        
 
         BasicResponse<Connector> response = new();
-        Connector _connector;
-        ConnectorDetail _connectorDetail;
-        _connector = new Connector
-        {
-            Status = request.Status ? (int)Status.ENABLED : (int)Status.DISABLED,
-            Name = request.ConnectorName,
-            //IsForADSS = request.IsForADSS == 1,
-            Provider = request.ProviderParams.AttributeKey,
-            Identifier = request.ProviderParams.AttributeName,
-            HMAC = request.ProviderParams.HMAC ?? DatabaseStaticData.HMAC,
-            CreatedBy = request.UserEmail ?? request.ProviderParams.CreatedBy ?? DatabaseStaticData.ADMIN,
-            LastModifiedBy = request.UserEmail ?? request.ProviderParams.LastModifiedBy ?? DatabaseStaticData.ADMIN,
-            LastModifiedOn = request.ProviderParams.LastModifiedOn ?? DateTime.Now,
-            ConnectorDetail = new List<ConnectorDetail>(),
-
-        };
-        request.ProviderParams.ConnectionProviderParameter.ForEach((item) =>
-        {
-            _connectorDetail = new ConnectorDetail
-            {
-                ConnectorId = _connector.Name,
-                AttributeName = item.AttributeKey,
-                AttributeValue = item.AttributeValue != null ? item.AttributeValue.Trim() : string.Empty,
-                //SortOrder = item.SortOrder,
-                Type = item.Type,
-                CreatedBy = request.UserEmail ?? item.CreatedBy ?? DatabaseStaticData.ADMIN,
-                LastModifiedBy = request.UserEmail ?? item.LastModifiedBy ?? DatabaseStaticData.ADMIN,
-                LastModifiedOn = item.LastModifiedOn ?? DateTime.Now,
-                HMAC = item.HMAC ?? DatabaseStaticData.HMAC,
-            };
-
-            _connector.ConnectorDetail.Add(_connectorDetail);
-        });
-
-        await _connectorCommandRepository.UpdateWithRelationAsync(_connector);
 
         if (await _unitOfWork.CommitAsync(cancellationToken))
         {
